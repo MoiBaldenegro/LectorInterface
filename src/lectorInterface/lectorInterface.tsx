ce297cba58866a3004bfd5def6f763a6bd0755a2
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-02-19 10:15:08 -0600

    first commit-m

---
+++ src/lectorInterface/lectorInterface.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function LectorInterface() {
+  return (
+    <div className="container">
+      <main>
+        <section>
+          <h1>RESPONSE</h1>
+        </section>
+      </main>
+    </div>
+  );
+}
