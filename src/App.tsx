54433c8cf95491f0539fdbd77a3b7dc8d9e499ea
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-02-19 21:08:35 -0600

    update project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { AcquisitionStarted } from '@digitalpersona/devices'
+
+function App() {
+
+  return (
+    <>
+      <h1>App</h1>
+    </>
+  )
+}
+
+export default App
